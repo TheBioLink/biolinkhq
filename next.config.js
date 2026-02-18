fa6f40b695a41468f8242da2829182f7305a8bd9
Solace <monkdev9@gmail.com>
2026-02-18 21:29:37 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // Render + proxy breaks Server Actions host validation
     serverActions: false,
   },
 
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "linktree13.s3.amazonaws.com",
-      },
+      { protocol: "https", hostname: "*.googleusercontent.com" },
+      { protocol: "https", hostname: "linktree13.s3.amazonaws.com" },
     ],
   },
 };
