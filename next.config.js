b9724e3f66e7f58e45b48a0b255f19b69398ac70
Solace <monkdev9@gmail.com>
2026-02-18 17:43:12 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    // Required for Render / reverse proxy environments
+    trustHostHeader: true,
+  },
+
   images: {
     remotePatterns: [
       {
-        hostname: '*.googleusercontent.com'
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
       },
       {
+        protocol: 'https',
         hostname: 'linktree13.s3.amazonaws.com',
       },
     ],
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
