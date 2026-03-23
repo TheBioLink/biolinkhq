b4806f7eee6c5b19bda5ec4303fa474fba37d7b7
Solace <monkdev9@gmail.com>
2026-03-23 22:56:24 +0000

    Create handleReferral.js

---
+++ src/libs/handleReferral.js
@@ -0,0 +1,36 @@
+import fetch from "node-fetch";
+import { User } from "../models/User.js";
+
+export async function handleReferralPurchase(user, plan) {
+  if (!user.referredBy) return;
+
+  const referrer = await User.findOne({
+    referralCode: user.referredBy,
+  });
+
+  if (!referrer) return;
+
+  referrer.referralEarnings.push({
+    referredUser: user.name,
+    plan,
+    timestamp: new Date(),
+  });
+
+  await referrer.save();
+
+  // 🔥 webhook
+  if (process.env.WEBHOOK_REF) {
+    await fetch(process.env.WEBHOOK_REF, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        referred_by: referrer.name,
+        referred_user: user.name,
+        plan: plan,
+        used_credits: user.subscription.startedWithCredits,
+      }),
+    });
+  }
+}
