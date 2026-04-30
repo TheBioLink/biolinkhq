9427fb1fd1f002a6641146847499f5717c3a7409
solace <monkdev9@gmail.com>
2026-04-30 19:28:37 +0100

    Refactor layout.js to move body content inside html

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,16 +9,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <body>
+        {children}
+
+        {/* Google AdSense (global load) */}
         <Script
           async
           strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8336311096274398"
           crossOrigin="anonymous"
         />
-      </head>
-      <body>
-        {children}
       </body>
     </html>
   );
